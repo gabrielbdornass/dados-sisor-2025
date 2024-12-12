aa698c16448340fe02d164cb4454e0255622e8a9
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-12-12 12:15:25 -0300

    Test api call with X-Forwarded-For
    Pass bh ip address
    See https://github.com/splor-mg/dados-sisor-2025/issues/9

--- scripts/extract.py
+++ scripts/extract.py
@@ -15,15 +15,22 @@ def extract_resource(resource_name: str, descriptor: str = 'datapackage.yaml'):
     PROXY_PWD = os.environ.get('PROXY_PWD')
     package = Package(descriptor)
     resource = package.get_resource(resource_name)
-    proxies = {
-        f'http://{PROXY_USER}:{PROXY_PWD}@proxycamg.prodemge.gov.br:8080'
-    }
+    # http://10.100.127.232:8080
+    # proxies = {
+    #     'http': f'http://{PROXY_USER}:{PROXY_PWD}@http://proxy.prodemge.gov.br:8080',
+    #     'https': f'http://{PROXY_USER}:{PROXY_PWD}@http://proxy.prodemge.gov.br:8080',
+    # }
     res = requests.post(resource.custom['api_url'],
-                        proxies=proxies,
-                        headers = {'User-Agent': 'splor'},
+                        # proxies=proxies,
+                        headers = {'User-Agent': 'splor',
+                                   'X-Forwarded-For': '187.113.201.87',
+                                   },
                         data = resource.custom['payload'],
                         stream = True)
+    breakpoint()
     res.raise_for_status()
 
     with open(resource.path, 'wb') as file:
         shutil.copyfileobj(res.raw, file)
+
+extract_resource('base_orcam_despesa_item_fiscal')
